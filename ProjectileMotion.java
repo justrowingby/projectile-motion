29105c4d081fc016a73dc0fc1422e23a448fc746
Rowan Emma Switzer <rowswitzer@gmail.com>
2017-10-20 15:14:39 -0500

    Added support for scientific notation, giving outputs at the lowest precision of the inputs.

--- ProjectileMotion.java
+++ ProjectileMotion.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-import java.lang.Math.*;
+import java.lang.Math;
+import java.math.*;
 
 public class ProjectileMotion{
     //inputs: time in air, horiz distance
@@ -7,14 +8,45 @@ public class ProjectileMotion{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.println("Please print three space separated values:");
-        System.out.println("the horizontal distance travelled, the time");
-        System.out.println("it spent in the air, and the absolute value");
-        System.out.println("of the gravity on your planet in m/s^2.");
-        double horizDis = in.nextDouble();
-        double time = in. nextDouble();
-        double gravity = -1 * in.nextDouble();
-        double[] results = calculations(horizDis, time, gravity);
-        System.out.println(results[0] + " " + results[1] + " " + results[2]);
+        System.out.println("the horizontal distance travelled in meters,");
+        System.out.println("the time spent in the air in seconds, and ");
+        System.out.println("the absolute value of the gravity on your ");
+        System.out.println("planet in m/s^2.");
+
+        String input1 = in.next();
+        String input2 = in.next();
+        String input3 = in.next();
+
+        BigDecimal horizDis = new BigDecimal(input1);
+        BigDecimal time = new BigDecimal(input2);
+        BigDecimal gravity = new BigDecimal(input3);
+
+        double dx = horizDis.doubleValue();
+        double dt = time.doubleValue();
+        double g = -1 * gravity.doubleValue();
+
+        if(input1.indexOf('.') == -1){ //if a decimal is not present in the string
+            horizDis = horizDis.stripTrailingZeros();
+        }
+        if(input2.indexOf('.') == -1){ //if a decimal is not present in the string
+            time = time.stripTrailingZeros();
+        }
+        if(input3.indexOf('.') == -1){ //if a decimal is not present in the string
+            gravity = gravity.stripTrailingZeros();
+        }
+
+        int[] precision = {horizDis.precision(), time.precision(), gravity.precision()};
+        int truePrecision = Integer.MAX_VALUE;
+        for(int p : precision){
+            if(p < truePrecision){
+                truePrecision = p;
+            }
+        }
+
+        double[] results = calculations(dx, dt, g);
+        System.out.println("Initial velocity: " + new BigDecimal(results[0], new MathContext(truePrecision, RoundingMode.HALF_UP)));
+        System.out.println("Angle of launch in radians: " + new BigDecimal(results[1], new MathContext(truePrecision, RoundingMode.HALF_UP)));
+        System.out.println("Maximum height: " + new BigDecimal(results[2], new MathContext(truePrecision, RoundingMode.HALF_UP)));
     }
     private static double[] calculations(double dx, double dt, double g){
         double vx = dx / dt;
@@ -24,5 +56,12 @@ public class ProjectileMotion{
         double vi = Math.sqrt(vx*vx + vy*vy);
         double[] results = new double[] {vi, theta, dy};
         return results;
+    }
+    private static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
